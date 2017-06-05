ac98ede16f07ed69cbc34d31c31136081d5cb8ba
Lordmatics <niallmaple@yahoo.co.uk>
2017-06-05 15:45:00 +0100

    Initial commit

---
+++ Source/AudioProject/AudioProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "AudioProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AUDIOPROJECT_API AAudioProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
